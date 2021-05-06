b32bd42bab48d794d6b524b8b8ed5f2227a3c39c
yassar123 <yassarali1999@gmail.com>
2021-05-06 21:25:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1 data-ns-test="project-name">ugkuhhf</h1>
-      <p data-ns-test="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, labore neque sit culpa quos consequatur reiciendis molestiae suscipit quasi! Quas odio incidunt, vitae, nesciunt officia dolor distinctio temporibus blanditiis deleniti minus laboriosam animi fuga.</p>
+      <h1 data-ns-test="project-name">Project Name</h1>
+      <p data-ns-test="project-description">Project Description.</p>
     </div>
   )
 }
