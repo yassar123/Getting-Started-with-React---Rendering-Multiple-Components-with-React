1710efe80f119e440f5d07b6d9bfc28ea32b4386
yassar123 <yassarali1999@gmail.com>
2021-05-06 21:21:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">ugkuhhf</h1>
+      <p data-ns-test="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, labore neque sit culpa quos consequatur reiciendis molestiae suscipit quasi! Quas odio incidunt, vitae, nesciunt officia dolor distinctio temporibus blanditiis deleniti minus laboriosam animi fuga.</p>
+    </div>
   )
 }
 
